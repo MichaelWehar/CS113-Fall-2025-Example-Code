18aa96ab5ae9d1eb807bb43cb3e50386bb5a70c2
Michael Wehar <dff72324@gmail.com>
2025-09-16 16:26:23 -0400

    Added another methods example

---
+++ lecture5/Methods2.java
@@ -0,0 +1,39 @@
+public class Methods2 {
+	public static void printCH() {
+		System.out.print("ch");
+	}
+	public static void printOC() {
+		System.out.print("oc");
+	}
+	public static void printOL() {
+		System.out.print("ol");
+	}
+	public static void printAT() {
+		System.out.print("at");
+	}
+	public static void printE() {
+		System.out.print("e");
+	}
+	public static void printVA() {
+		System.out.print("va");
+	}
+	public static void printNI() {
+		System.out.print("ni");
+	}
+	public static void printLL() {
+		System.out.print("ll");
+	}
+	public static void printA() {
+		System.out.print("a");
+	}
+	public static void main(String[] args) {
+		printAT();
+		System.out.println();
+		printAT();
+		printE();
+		System.out.println();
+		printCH();
+		printAT();
+		System.out.println();
+	}
+}
