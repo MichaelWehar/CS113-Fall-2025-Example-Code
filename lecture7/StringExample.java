eed5389f33f8cb5a991a8e251ec92e010ed27465
Michael Wehar <dff72324@gmail.com>
2025-09-23 15:42:53 -0400

    Added more to String Example

--- lecture7/StringExample.java
+++ lecture7/StringExample.java
@@ -1,20 +1,35 @@
 public class StringExample {
 	public static boolean containsDisPrefix(String x) {
-		if(x.length() < 3) {
+		String lowerX = x.toLowerCase();
+		if(lowerX.length() < 3) {
 			return false;
 		} else {
-			if(x.charAt(0) == 'd' && x.charAt(1) == 'i' && x.charAt(2) == 's') {
+			if(lowerX.charAt(0) == 'd' && lowerX.charAt(1) == 'i' && lowerX.charAt(2) == 's') {
 				return true;
 			} else {
 				return false;
 			}
 		}
 	}
+	public static boolean containsDisPrefix2(String x) {
+		String lowerX = x.toLowerCase();
+		if(lowerX.length() < 3) {
+			return false;
+		} else {
+			String prefix = lowerX.substring(0, 3);
+			// if(prefix.equals("dis")) {
+			// 	return true;
+			// } else {
+			// 	return false;
+			// }
+			return prefix.equals("dis");
+		}
+	}
 	public static void main(String[] args) {
 		String word1 = "disapprove";
 		String word2 = "computer";
-		boolean result1 = containsDisPrefix(word1);
-		boolean result2 = containsDisPrefix(word2);
+		boolean result1 = containsDisPrefix2(word1);
+		boolean result2 = containsDisPrefix2(word2);
 		System.out.println("Result 1: " + result1);
 		System.out.println("Result 2: " + result2);
 	}
