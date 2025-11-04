d5686a1c90839f6808f150dcc964874ffa0b8b26
Michael Wehar <dff72324@gmail.com>
2025-11-04 15:04:24 -0500

    Update

--- lecture17/Lab/Board.java
+++ lecture17/Lab/Board.java
@@ -2,8 +2,10 @@ public class Board {
   // Member Variables
   public int treasureX;
   public int treasureY;
+  public int width;
+  public int height;
   // Constructor
-  public Board() {
+  public Board(int w, int h) {
     // call set random position
   }
   public void setRandomPosition() {
