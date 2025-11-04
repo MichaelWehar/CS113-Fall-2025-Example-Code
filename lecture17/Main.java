5e1b1050228549fa56acc3e2bc6463685cd2db97
Michael Wehar <dff72324@gmail.com>
2025-11-04 14:52:20 -0500

    Added lecture 17

---
+++ lecture17/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		Pair obj1 = new Pair(10, 5);
+		Pair obj2 = new Pair(2, 7);
+		System.out.println(obj1.getX());
+		System.out.println(obj2.getY());
+	}
+}
---
+++ lecture17/Pair.java
@@ -0,0 +1,17 @@
+public class Pair {
+	// Member variables
+	public int x;
+	public int y;
+	// Constructor
+	public Pair(int a, int b) {
+		x = a;
+		y = b;
+	}
+	// Methods
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+}
