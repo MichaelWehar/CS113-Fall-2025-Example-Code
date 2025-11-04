59ee800c303db1b3c6b01ad89cbb9913f803aacc
Michael Wehar <dff72324@gmail.com>
2025-11-04 14:57:44 -0500

    Added starter code

---
+++ lecture17/Lab/Board.java
@@ -0,0 +1,17 @@
+public class Board {
+  // Member Variables
+  public int treasureX;
+  public int treasureY;
+  // Constructor
+  public Board() {
+    // call set random position
+  }
+  public void setRandomPosition() {
+    // fill this in
+  }
+  public int guessPosition(int x, int y) {
+    // fill this in
+    // return how many spaces
+    // away from the treasure
+  }
+}
